107338f7ee0fff16e8e433b6d5b218c65c800d1d
robertraya <robertjamesraya@gmail.com>
2020-11-01 16:30:58 -0700

    Update settings.py

--- website/praisetheflesh/realreal/settings.py
+++ website/praisetheflesh/realreal/settings.py
@@ -122,12 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
+
     
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Security settings
 # helps protect against cross site scripting
