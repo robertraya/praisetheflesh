5cdcd055695ca2eaaa6e31320f7722f373c0627a
robertraya <robertjamesraya@gmail.com>
2020-11-01 17:11:26 -0700

    Update settings.py

--- praisetheflesh/realreal/settings.py
+++ praisetheflesh/realreal/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-     'praisetheflesh.apps.PraisetheFleshConfig',
+    'praisetheflesh.apps.PraisetheFleshConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
